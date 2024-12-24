66deedd4ebdfe47da88890edcbd2a8ddb8487b32
chrisokoriedev <okoriec01@gmail.com>
2024-12-24 23:39:55 +0100

    update

--- lib/presentation/splash/pages/splash.dart
+++ lib/presentation/splash/pages/splash.dart
@@ -7,7 +7,7 @@ class SplashPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Image.asset(''),
+        child: Image.asset('assets/images/splash.jpg'),
       ),
     );
   }
