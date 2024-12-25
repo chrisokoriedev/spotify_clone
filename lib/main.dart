bb404168948a6105d00e93c9f98c7673912cde68
chrisokoriedev <okoriec01@gmail.com>
2024-12-25 06:51:18 +0100

    update gradle and kotlin version added theme cutbt

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:spotify_clone/core/config/router/app_router.dart';
 import 'package:spotify_clone/core/config/theme/app_theme.dart';
+import 'package:spotify_clone/presentation/choosemode/bloc/theme_cubit.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -21,17 +23,23 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ScreenUtilInit(
-      designSize: const Size(428, 926),
-      minTextAdapt: true,
-      splitScreenMode: false,
-      ensureScreenSize: true,
-      builder: (context, _) => MaterialApp.router(
-        title: 'Flutter Demo',
-        debugShowCheckedModeBanner: false,
-        theme: AppTheme.lightTheme,
-        routerConfig: AppRouter.routes,
-      ),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (_) => ThemeCubit()),
+      ],
+      child: BlocBuilder<ThemeCubit, ThemeMode>(
+          builder: (context, mode) => ScreenUtilInit(
+                designSize: const Size(428, 926),
+                minTextAdapt: true,
+                splitScreenMode: false,
+                ensureScreenSize: true,
+                builder: (context, _) => MaterialApp.router(
+                  title: 'Flutter Demo',
+                  debugShowCheckedModeBanner: false,
+                  theme: AppTheme.lightTheme,
+                  routerConfig: AppRouter.routes,
+                ),
+              )),
     );
   }
 }
