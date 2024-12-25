35c3d18e67a25b2412e5efa0769242b425935136
chrisokoriedev <okoriec01@gmail.com>
2024-12-25 06:29:07 +0100

    updated run app with hyrdated bloc implementation

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,20 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:spotify_clone/core/config/router/app_router.dart';
 import 'package:spotify_clone/core/config/theme/app_theme.dart';
 
-void main() => runApp(const MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  HydratedBloc.storage = await HydratedStorage.build(
+    storageDirectory: kIsWeb
+        ? HydratedStorage.webStorageDirectory
+        : await getApplicationDocumentsDirectory(),
+  );
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
